0bcb57a366cd952e19a93f22b636535a7ceb4a60
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-03-20 17:23:09 +0900

    refactor: コメント追加

--- src/lib/fetchPrefectures.ts
+++ src/lib/fetchPrefectures.ts
@@ -1,6 +1,9 @@
 import { fetchToResas } from "./fetchToResas";
 import { PrefecturesRes, prefecturesResSchema } from "./prefecturesResSchema";
 
+/**
+ * RESAS APIにアクセスし、県名の一覧を取得する
+ */
 export const fetchPrefectures = async (): Promise<PrefecturesRes["result"]> => {
   const res = await fetchToResas<Record<string, never>>({
     apiPath: "a",
