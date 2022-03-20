15bf4ccc71b4d695cbbe9b28eeb3c00437f4c2e7
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-03-20 17:04:33 +0900

    feat: populationResSchemaのスキーマを定義

--- src/lib/populationResSchema.ts
+++ src/lib/populationResSchema.ts
@@ -1,3 +1,45 @@
 import { z } from "zod";
 
-export const populationResSchema = z.object({});
+const subDataSchema = z.object({
+  year: z.number(),
+  value: z.number(),
+  rate: z.number(),
+});
+
+export const populationResSchema = z.object({
+  message: z.nullable(z.string()),
+  result: z.object({
+    boundaryYear: z.number(),
+    data: z.array(
+      z.union([
+        z.object({
+          label: z.literal("総人口"),
+          data: z.array(
+            z.object({
+              year: z.number(),
+              value: z.number(),
+            })
+          ),
+        }),
+        z.object({
+          label: z.literal("年少人口"),
+          data: z.array(subDataSchema),
+        }),
+        z.object({
+          label: z.literal("生産年齢人口"),
+          data: z.array(subDataSchema),
+        }),
+        z.object({
+          label: z.literal("老年人口"),
+          data: z.array(subDataSchema),
+        }),
+        z.object({
+          label: z.string(),
+          data: z.array(subDataSchema),
+        }),
+      ])
+    ),
+  }),
+});
+
+export type PopulationRes = z.infer<typeof populationResSchema>;
