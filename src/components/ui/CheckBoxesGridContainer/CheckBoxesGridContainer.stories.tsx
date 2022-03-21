19fb319d48b6a97824bfed5e883e2a260b33f924
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-03-21 16:08:02 +0900

    feat: CheckBoxesGridContainerを実装

--- src/components/ui/CheckBoxesGridContainer/CheckBoxesGridContainer.stories.tsx
+++ src/components/ui/CheckBoxesGridContainer/CheckBoxesGridContainer.stories.tsx
@@ -1,8 +1,22 @@
-import { CheckBoxesGridContainer, CheckBoxesGridContainerProps} from "./CheckBoxesGridContainer";
+import {
+  CheckBoxesGridContainer,
+  CheckBoxesGridContainerProps,
+} from "./CheckBoxesGridContainer";
+import { CheckBox } from "../CheckBox/CheckBox";
 
 export default {
-	title: "ui/CheckBoxesGridContainer",
-	component: CheckBoxesGridContainer
-}
+  title: "ui/CheckBoxesGridContainer",
+  component: CheckBoxesGridContainer,
+};
 
-export const Default = (args: CheckBoxesGridContainerProps) => <CheckBoxesGridContainer {...args}/>
+export const Default = (args: CheckBoxesGridContainerProps) => (
+  <CheckBoxesGridContainer {...args}>
+    <CheckBox key={"box1"}>CheckBox1</CheckBox>
+    <CheckBox key={"box2"}>CheckBox2</CheckBox>
+    <CheckBox key={"box3"}>CheckBox3</CheckBox>
+    <CheckBox key={"box4"}>CheckBox4</CheckBox>
+    <CheckBox key={"box5"}>CheckBox5</CheckBox>
+    <CheckBox key={"box6"}>CheckBox6</CheckBox>
+    <CheckBox key={"box7"}>CheckBox7</CheckBox>
+  </CheckBoxesGridContainer>
+);
--- src/components/ui/CheckBoxesGridContainer/CheckBoxesGridContainer.tsx
+++ src/components/ui/CheckBoxesGridContainer/CheckBoxesGridContainer.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import styles from "./CheckBoxesGridContainer.module.scss";
 
-export type CheckBoxesGridContainerProps = {
-  
-};
+export type CheckBoxesGridContainerProps = {};
 
-export const CheckBoxesGridContainer: React.FC<CheckBoxesGridContainerProps> = ({
-  
-}) => {
-  return (
-    <>
-    </>
-  );
-};
+export const CheckBoxesGridContainer: React.FC<CheckBoxesGridContainerProps> =
+  ({ children }) => {
+    return (
+      <div className={styles["check-boxes-grid-container"]}>{children}</div>
+    );
+  };
