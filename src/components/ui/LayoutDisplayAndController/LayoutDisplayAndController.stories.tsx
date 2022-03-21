24df71e24c8fecf2c525a1e3d550fdafdee8167d
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-03-21 18:35:32 +0900

    feat: LayoutDisplayAndControllerを実装、忘れていたスナップショットを追加

--- src/components/ui/LayoutDisplayAndController/LayoutDisplayAndController.stories.tsx
+++ src/components/ui/LayoutDisplayAndController/LayoutDisplayAndController.stories.tsx
@@ -1,8 +1,17 @@
-import { LayoutDisplayAndController, LayoutDisplayAndControllerProps} from "./LayoutDisplayAndController";
+import {
+  LayoutDisplayAndController,
+  LayoutDisplayAndControllerProps,
+} from "./LayoutDisplayAndController";
 
 export default {
-	title: "ui/LayoutDisplayAndController",
-	component: LayoutDisplayAndController
-}
+  title: "ui/LayoutDisplayAndController",
+  component: LayoutDisplayAndController,
+};
 
-export const Default = (args: LayoutDisplayAndControllerProps) => <LayoutDisplayAndController {...args}/>
+export const Default = (args: LayoutDisplayAndControllerProps) => (
+  <LayoutDisplayAndController
+    {...args}
+    controller={<div style={{ background: "#ff9966" }}>Controller</div>}
+    display={<div style={{ background: "#33ccff" }}>Display</div>}
+  />
+);
