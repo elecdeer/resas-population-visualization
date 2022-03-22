f1932e3826279a28f785c7385d2f89817f20dbc0
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-03-22 21:48:57 +0900

    feat: Rechartsで正常に動作するように実装

--- src/components/model/PopulationChartDisplay/PopulationChartDisplay.tsx
+++ src/components/model/PopulationChartDisplay/PopulationChartDisplay.tsx
@@ -1,7 +1,6 @@
 import React, { useMemo } from "react";
 import { PrefectureCheckStates } from "../../../hooks/usePrefecturesState";
 import { PrefecturesRes } from "../../../lib/schema/prefecturesResSchema";
-import { PopulationLine } from "../PopulationLine/PopulationLine";
 import {
   CartesianGrid,
   Legend,
@@ -12,15 +11,18 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
+import { usePopulation } from "../../../hooks/usePopulation";
 
 export type PopulationChartDisplayProps = {
   prefectures: PrefecturesRes["result"];
   checkState: PrefectureCheckStates;
+  showEstimation?: boolean;
 };
 
 export const PopulationChartDisplay: React.VFC<PopulationChartDisplayProps> = ({
   prefectures,
   checkState,
+  showEstimation = false,
 }) => {
   const displayPrefectures = useMemo(
     () => prefectures.filter((pref) => checkState[pref.prefCode]),
@@ -30,113 +32,28 @@ export const PopulationChartDisplay: React.VFC<PopulationChartDisplayProps> = ({
     `displayPrefectures: ${displayPrefectures.map((pref) => pref.prefCode)}`
   );
 
+  const { data: populations } = usePopulation(displayPrefectures);
+
   const renderLines = () =>
-    displayPrefectures.map((pref) => (
-      <PopulationLine
-        prefCode={pref.prefCode}
+    populations?.map((pref) => (
+      <Line
+        dataKey={"value"}
+        data={pref.data}
         name={pref.prefName}
-        key={`prefecture-line-${pref.prefCode}`}
+        key={pref.prefCode}
       />
     ));
 
-  const data = [
-    {
-      year: 1960,
-      value: 5039206,
-    },
-    {
-      year: 1965,
-      value: 5171800,
-    },
-    {
-      year: 1970,
-      value: 5184287,
-    },
-    {
-      year: 1975,
-      value: 5338206,
-    },
-    {
-      year: 1980,
-      value: 5575989,
-    },
-    {
-      year: 1985,
-      value: 5679439,
-    },
-    {
-      year: 1990,
-      value: 5643647,
-    },
-    {
-      year: 1995,
-      value: 5692321,
-    },
-    {
-      year: 2000,
-      value: 5683062,
-    },
-    {
-      year: 2005,
-      value: 5627737,
-    },
-    {
-      year: 2010,
-      value: 5506419,
-    },
-    {
-      year: 2015,
-      value: 5381733,
-    },
-    {
-      year: 2020,
-      value: 5216615,
-    },
-    {
-      year: 2025,
-      value: 5016554,
-    },
-    {
-      year: 2030,
-      value: 4791592,
-    },
-    {
-      year: 2035,
-      value: 4546357,
-    },
-    {
-      year: 2040,
-      value: 4280427,
-    },
-    {
-      year: 2045,
-      value: 4004973,
-    },
-  ];
   return (
-    <ResponsiveContainer height={300} width={"100%"}>
+    <ResponsiveContainer height={400} width={"100%"}>
       <LineChart>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="year" />
+        <XAxis dataKey="year" allowDuplicatedCategory={false} />
         <YAxis />
         <Tooltip />
         <Legend layout="vertical" verticalAlign="top" align="right" />
-        {/*{renderLines()}*/}
-        <Line dataKey={"value"} data={data} name={"どこか"} />)
-        <PopulationLine prefCode={1} name={"北海道"} />
+        {renderLines()}
       </LineChart>
     </ResponsiveContainer>
   );
-
-  // return (
-  //   <YearlyLineChart>
-  //     {displayPrefectures.map((pref) => (
-  //       <PopulationLine
-  //         prefCode={pref.prefCode}
-  //         name={pref.prefName}
-  //         key={`prefecture-line-${pref.prefCode}`}
-  //       />
-  //     ))}
-  //   </YearlyLineChart>
-  // );
 };
--- src/hooks/usePopulation.ts
+++ src/hooks/usePopulation.ts
@@ -1,16 +1,64 @@
 import { PopulationRes } from "../lib/schema/populationResSchema";
 import useSWR, { Fetcher } from "swr";
 import { PopulationParam } from "../lib/schema/populationParamSchema";
+import { PrefecturesRes } from "../lib/schema/prefecturesResSchema";
 
-const fetcher: Fetcher<PopulationRes["result"], [string, PopulationParam]> = (
-  url,
-  param
-) =>
-  fetch(url, {
-    method: "POST",
-    body: JSON.stringify(param),
-  }).then((res) => res.json() as Promise<PopulationRes["result"]>);
+type ResultItem = {
+  year: number;
+} & Record<number, number>;
 
-export const usePopulation = (param: PopulationParam | null) => {
-  return useSWR(param ? ["/api/population", param] : null, fetcher);
+const fetcher: Fetcher<
+  (PrefecturesRes["result"][number] & {
+    label: string | undefined;
+    data: PopulationRes["result"]["data"][number]["data"] | undefined;
+  })[],
+  {
+    url: string;
+    prefectures: PrefecturesRes["result"];
+    label: string;
+    showEstimation?: boolean;
+  }
+> = async ({ url, prefectures, showEstimation = false, label }) => {
+  return await Promise.all(
+    prefectures.map(async (pref) => {
+      const query: PopulationParam = {
+        prefCode: pref.prefCode,
+        cityCode: "-",
+      };
+
+      const res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(query),
+      });
+      const result = (await res.json()) as PopulationRes["result"];
+
+      const groupData = result.data.find((item) => item.label === label);
+
+      return {
+        ...pref,
+        label: groupData?.label,
+        data: showEstimation
+          ? groupData?.data
+          : groupData?.data.filter(
+              (yearItem) => yearItem.year <= result.boundaryYear
+            ),
+      };
+    })
+  );
+};
+
+export const usePopulation = (
+  prefectures: PrefecturesRes["result"],
+  label: string = "総人口"
+) => {
+  return useSWR(
+    prefectures
+      ? {
+          url: "/api/population",
+          prefectures: prefectures,
+          label: label,
+        }
+      : null,
+    fetcher
+  );
 };
